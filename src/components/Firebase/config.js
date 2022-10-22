32addbed672341e4ae21631158236a2e15e2e170
Batuhan <105635441+batuhanysengul@users.noreply.github.com>
2022-10-22 18:42:50 +0300

    Update config.js

--- src/components/Firebase/config.js
+++ src/components/Firebase/config.js
@@ -3,7 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const config = {
-  //firebase config goes here
+    apiKey: "AIzaSyD3TgIhfBRmie9Ggl8OifXAZRQb81w6xGA",
+  authDomain: "chatapp-db86d.firebaseapp.com",
+  projectId: "chatapp-db86d",
+  storageBucket: "chatapp-db86d.appspot.com",
+  messagingSenderId: "445919880432",
+  appId: "1:445919880432:web:482b2c596b55b7f96d8e35",
+  measurementId: "G-JJGWW5F999",
 };
 
 const firebaseApp = firebase.initializeApp(config)
